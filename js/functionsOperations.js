6ef0da47a1b3a3bb1f3d07a5964f9d99b47dd231
Leonardo José <113205583+l30n44rd0@users.noreply.github.com>
2023-04-28 11:31:37 -0300

    movi functionsOperations para pasta js e exclui pasta functions

---
+++ js/functionsOperations.js
@@ -0,0 +1,83 @@
+// module.exports = { sum, subtract, multiply, divide, ptnc, factorial, sqrt, isEven: numeroEImpar, isOdd: numeroEPar, isPrime, changeOperation };
+
+function sum(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function factorial(n) {
+  if (n < 0) 
+  return -1;
+  else if (n == 0) 
+  return 1;
+  else {
+    return (n * factorial(n - 1));
+  }
+}
+
+function sqrt(n) {
+  return Math.sqrt(n);
+}
+
+function ptnc(b, e) {
+  return Math.pow(b, e);
+}
+
+function numeroEPar(num) {
+  if (num % 2 !== 0) {
+    console.log(`${num} é ímpar`);
+    return "Não";
+  } else {
+    console.log(`${num} é par`);
+     return "Sim";
+    }
+}
+
+function numeroEImpar(num) {
+  if(num % 2 === 0) {
+    console.log(`${num} é par`);
+        return "Não";
+    } else{
+      console.log(`${num} é ímpar`);
+      return "Sim";
+    }
+  }
+  
+  function isPrime(num) {
+    if (num <= 1) {
+      console.log(`${num} NÃO é primo`);
+      return "Não";
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        console.log(`${num} NÃO é primo`);
+        return "Não";
+      }
+    }
+    console.log(`${num} é primo`);
+    return "Sim";
+
+  }
+
+// Execução dos testes:
+// exports.sum = sum;
+// exports.subtract = subtract;
+// exports.multiply = multiply;
+// exports.divide = divide;
+// exports.ptnc = ptnc;
+// exports.factorial = factorial;
+// exports.sqrt = sqrt;
+// exports.numeroEImpar = numeroEImpar;
+// exports.numeroEPar = numeroEPar;
+// exports.isPrime = isPrime;
